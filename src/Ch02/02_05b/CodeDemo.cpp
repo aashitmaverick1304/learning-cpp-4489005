d3a9ecb8d53b471adb5acba2c2769fb7c3a812d2
Aashit Hinge <112811139+aashitmaverick1304@users.noreply.github.com>
2025-04-05 15:06:18 +0000

    add a commit

--- src/Ch02/02_05b/CodeDemo.cpp
+++ src/Ch02/02_05b/CodeDemo.cpp
@@ -3,10 +3,15 @@
 // Type inference with auto, by Eduardo Corpeño 
 
 #include <iostream>
+using namespace std;
 #include <typeinfo>
-
 int main(){
-    
-    std::cout << std::endl << std::endl;
+    auto i=23456789;
+    auto j=23.4;
+    auto k=true;
+    auto m="Aashit";
+    cout<<"type of i is: "<<typeid(i).name();
+    cout<<"type of m is: "<<typeid(m).name(); // Pkc - pointer constant character
+    cout << endl << endl;
     return (0);
 }
--- src/Ch02/02_08b/CodeDemo.cpp
+++ src/Ch02/02_08b/CodeDemo.cpp
@@ -3,9 +3,20 @@
 // Arrays, by Eduardo Corpeño 
 
 #include <iostream>
-
+using namespace std;
 int main(){
-    
-    std::cout << std::endl << std::endl;
+    const int size=4;
+    int age[size];
+    float temperature[]={23.3,34.56,12.00,12.23};
+
+    age[0]=23;
+    age[1]=34;
+    age[2]=54;
+    age[3]=20;
+
+    for(int i=0;i<4;i++){
+        // cout<<age[i]<<endl;
+        cout<<temperature[i]<<endl;
+    }
     return (0);
 }
