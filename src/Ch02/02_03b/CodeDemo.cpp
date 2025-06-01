1e3fff985c6778abcb72f0340e010ffdb5eb26f1
Aashit Hinge <112811139+aashitmaverick1304@users.noreply.github.com>
2025-06-01 06:31:28 +0000

    add a new commit

--- src/Ch02/02_03b/CodeDemo.cpp
+++ src/Ch02/02_03b/CodeDemo.cpp
@@ -3,10 +3,15 @@
 // Using Variables, by Eduardo Corpeño 
 
 #include <iostream>
-
+using namespace std;
+int a,b=5;
 int main(){
     std::cout << "Hi There!" << std::endl;
-    
-    std::cout << std::endl << std::endl;
+    bool my_flag;
+    my_flag=false;
+    a=10;
+    cout<<"a is: "<<a<<endl;
+    cout<<"b is: "<<b<<endl;
+    cout<<"a is: "<<my_flag<<endl;
     return (0);
 }
