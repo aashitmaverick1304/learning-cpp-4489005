a83f9bb4552106df36ce88bcaefaaf3534f475e3
Aashit Hinge <112811139+aashitmaverick1304@users.noreply.github.com>
2025-06-02 14:34:15 +0000

    add a commit

--- src/Ch03/03_01b/CodeDemo.cpp
+++ src/Ch03/03_01b/CodeDemo.cpp
@@ -4,9 +4,20 @@
 
 #include <iostream>
 #include <string>
+using namespace std;
 
+enum cow_purpose {dairy , hide , meat , pet};
+
+struct cow{
+    string name;
+    int age;
+    cow_purpose purpose;
+};
 int main(){
-    
-    std::cout << std::endl << std::endl;
+    cow my_cow;
+    my_cow.name="betsy";
+    my_cow.age=7;
+    my_cow.purpose=cow_purpose::dairy;
+    cout << endl << endl;
     return (0);
 }
